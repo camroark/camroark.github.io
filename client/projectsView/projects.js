68672c8d1d3bfa4aa98f4741bd1e8d8444200e41
Cameron Roark <cam@cameronroark.com>
2016-04-26 13:11:06 -0700

    [chore] finish content

--- client/projectsView/projects.js
+++ client/projectsView/projects.js
@@ -1,6 +1,27 @@
 myApp
 .controller('projectsCtrl', function ($scope){
-   $scope.openLeftMenu = function() {
-    $mdSidenav('left').toggle();
+   $scope.openLeftMenu = function () {
+      $mdSidenav('left').toggle();
    };
+
+   // Filter
+   $scope.searchParty = ['Angular2', 'Ionic', 'Node.js', 'Express', 'API\'s', 'Neo4j'];
+   $scope.lunchLine = ['AngularJS', 'Node.js', 'Express', 'API\'s', 'MongoDB'];
+   $scope.drivewayShare = ['AngularJS', 'Node.js', 'Express', 'SQL'];
+
+   $scope.filter = function (project) {
+      var techList;
+      if(project === 'searchParty') {
+         techList = $scope.searchParty;
+      } else if (project === 'lunchline') {
+         techList = $scope.lunchLine;
+      } else if (project === 'drivewayShare') {
+         techList = $scope.drivewayShare;
+      }
+      var techItems = [];
+      
+
+   }
+
+
 });
